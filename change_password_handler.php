c6f395f3d32a5ebc5cd21d08b642599c077dfc27
ninjgalla <ninjgalla@gmail.com>
2024-03-15 14:43:50 +0800

    Update change_password_handler.php

--- change_password_handler.php
+++ change_password_handler.php
@@ -5,7 +5,7 @@ session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['username'])) {
     // Redirect the user to the login page
-    header("Location: login.php");
+    header("Location: index.php");
     exit; // Stop further execution
 }
 
