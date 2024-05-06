372ad5aabbcbc6ae96a9ab1f833442ff2a4e6069
ninjgalla <ninjgalla@gmail.com>
2024-05-06 15:03:59 +0800

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -29,7 +29,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Dashboard</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
 
