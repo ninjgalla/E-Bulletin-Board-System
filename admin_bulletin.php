cf4217b1aef59c1c40b59b29ddf9b062d36244f3
ninjgalla <ninjgalla@gmail.com>
2024-05-06 15:04:12 +0800

    Update admin_bulletin.php

--- admin_bulletin.php
+++ admin_bulletin.php
@@ -23,7 +23,7 @@ $username = $_SESSION['username'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>EBBS</title>
     <style>
 
         html, body {
