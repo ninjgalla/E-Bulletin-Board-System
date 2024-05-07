d39203c92f45b768e75f4622476b764932413a5f
ninjgalla <ninjgalla@gmail.com>
2024-05-07 11:54:04 +0800

    Update admin_profile_settings.php

--- admin_profile_settings.php
+++ admin_profile_settings.php
@@ -209,11 +209,12 @@ $username = $_SESSION['username'];
         <div>
             
             <a href="admin_upload.php">Upload</a>
+            <a href="admin_bulletin_feed.php">Bulletin Feed</a>
             <a href="admin_archive.php">Archive</a>
             <a href="admin_profile_settings.php">Profile</a>
             <a href="logout.php">Logout</a>
         </div>
-    </div>
+</div>
 
 <!-- Icons below the navbar -->
 <div class="navbar-icons">
